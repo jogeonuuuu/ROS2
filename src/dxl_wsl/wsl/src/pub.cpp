59f3d4b217cd3c5c85683d7f23709ccc59bcecbc
jogeonuuuu <wnrdjaos3@gmail.com>
2025-06-13 23:25:05 +0900

    Update pub.cpp

--- src/dxl_wsl/wsl/src/pub.cpp
+++ src/dxl_wsl/wsl/src/pub.cpp
@@ -7,44 +7,6 @@
 #include <fcntl.h>
 #include <stdio.h>
 
-/*bool kbhit() {
-    struct termios oldt, newt;
-    int ch;
-    int oldf;
-
-    tcgetattr(STDIN_FILENO, &oldt); // 현재 터미널 속성 저장
-    newt = oldt;
-    newt.c_lflag &= ~(ICANON | ECHO); // 버퍼 없이 입력, 입력 문자 출력 안함
-    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-    oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
-    fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK); // 논블로킹 모드
-
-    ch = getchar(); // 키 입력 읽기
-    tcsetattr(STDIN_FILENO, TCSANOW, &oldt); // 원래 터미널 속성 복원
-    fcntl(STDIN_FILENO, F_SETFL, oldf); // 원래 파일 속성 복원
-
-    if (ch != EOF) {
-        ungetc(ch, stdin); // 읽은 키를 다시 입력 스트림으로 돌려놓음
-        return true;
-    }
-
-    return false;
-}
-
-char getch() {
-    struct termios oldt, newt;
-    char ch;
-
-    tcgetattr(STDIN_FILENO, &oldt); // 현재 터미널 속성 저장
-    newt = oldt;
-    newt.c_lflag &= ~(ICANON | ECHO); // 버퍼 없이 입력, 입력 문자 출력 안함
-    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-    ch = getchar(); // 실제 키 입력
-    tcsetattr(STDIN_FILENO, TCSANOW, &oldt); // 원래 터미널 속성 복원
-
-    return ch;
-}*/
-
 int main(int argc, char * argv[])
 {
     rclcpp::init(argc, argv);
@@ -59,36 +21,10 @@ int main(int argc, char * argv[])
     int goal1=0, goal2=0;
     while(rclcpp::ok())
     {
-        /*if (kbhit())
-        {
-            char c = getch();
-            switch(c)
-            {
-            case 's': goal1 = 0; goal2 = 0; break;
-            case ' ': goal1 = 0; goal2 = 0; break;
-            case 'f': goal1 = 50; goal2 = -50; break;
-            case 'b': goal1 = -50; goal2 = 50; break;
-            case 'l': goal1 = -50; goal2 = -50; break;
-            case 'r': goal1 = 50; goal2 = 50; break;
-            default : goal1 = 0; goal2 = 0; break;
-            }
-        }
-        // generate accel and decel motion
-        if(goal1>vel1) vel1 += 5;
-        else if(goal1<vel1) vel1 -= 5;
-        else vel1 = goal1;
-        if(goal2>vel2) vel2 += 5;
-        else if(goal2<vel2) vel2 -= 5;
-        else vel2 = goal2;
-        vel.x = vel1;
-        vel.y = vel2;*/
-        /*
         std::cout << "input >> ";
-        std::string str;
-        std::cin >> str;
-        if(!str.empty())
-        {
-            c=str[0];
+        char c='p';
+        std::cin >> c;
+        if(c!='p') {
             switch(c)
             {
             case ' ': vel.x = 0; vel.y = 0; break; //정지
@@ -98,20 +34,7 @@ int main(int argc, char * argv[])
             case 'd': vel.x = 50; vel.y = 50; break; //우회전
             default : vel.x = 0; vel.y = 0; break;
             }
-        }*/
-        std::cout << "input >> ";
-        char c='p';
-        std::cin >> c;
-        if(c!='p')
-        {switch(c)
-        {
-        case ' ': vel.x = 0; vel.y = 0; break; //정지
-        case 'w': vel.x = 50; vel.y = -50; break; //전진
-        case 's': vel.x = -50; vel.y = 50; break; //후진
-        case 'a': vel.x = -50; vel.y = -50; break; //좌회전
-        case 'd': vel.x = 50; vel.y = 50; break; //우회전
-        default : vel.x = 0; vel.y = 0; break;
-        }}
+        }
 
         RCLCPP_INFO(node->get_logger(), "Publish: %lf,%lf", vel.x, vel.y);
         mypub->publish(vel);
